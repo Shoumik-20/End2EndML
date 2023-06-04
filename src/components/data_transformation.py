e13dea88f55b9a5aa1b9ba4e8e88261679557f8a
Shoumik-20 <shoumikn@usc.edu>
2023-06-03 23:03:22 -0700

    Updating data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,7 +16,7 @@ from src.logger import logging
 from src.utils import save_object
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"processor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
